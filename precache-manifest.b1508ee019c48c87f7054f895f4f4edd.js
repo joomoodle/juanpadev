8b25ad8b2797c45057801249c516fb602a40fe7c
joomoodle <juan_pablo_ing@outlook.es>
2020-03-22 11:28:48 -0600

    Updates

---
+++ precache-manifest.b1508ee019c48c87f7054f895f4f4edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee11bc7130a482942e2b35d7d4ba8160",
+    "url": "/juanpadev/index.html"
+  },
+  {
+    "revision": "c67413982af84a025d0e",
+    "url": "/juanpadev/static/css/main.9323cafc.chunk.css"
+  },
+  {
+    "revision": "21d4c516330ceb3b24e4",
+    "url": "/juanpadev/static/js/2.3df71d8d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/juanpadev/static/js/2.3df71d8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c67413982af84a025d0e",
+    "url": "/juanpadev/static/js/main.939ff306.chunk.js"
+  },
+  {
+    "revision": "07ebfb228a5eaee2f29c",
+    "url": "/juanpadev/static/js/runtime-main.5723231f.js"
+  }
+]);
